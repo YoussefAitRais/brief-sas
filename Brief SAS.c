ad2c50668bdcb3b3932182fa59748e1df12d60a3
Youssef Ait Rais <copybuisness007@gmail.com>
2024-10-29 18:31:33 +0200

    Update Brief SAS.c

--- Brief SAS.c
+++ Brief SAS.c
@@ -3,6 +3,8 @@
 
 #define max 30
 
+// declaration des variable
+
 typedef struct date
 {
 
@@ -22,13 +24,16 @@ typedef struct Gtaches
 
 } Gtaches;
 
+
+//ajouter un tache
+
 void ajoutetache(Gtaches tache[],int i)
 {
 
 
 
     printf("Ajouter Un Tache  %d \n",i+1);
-    printf("------------espace---------- \n");
+    printf("||------------espace----------|| \n");
 
     printf("Ajouter Un titre \n");
 
@@ -52,96 +57,111 @@ void ajoutetache(Gtaches tache[],int i)
     printf("Ajouter une priorite : \n");
     scanf ("%s", &tache[i].priorite);
 
+}
+// Fonction pour modifier une t√¢che
+
+void modifietache (Gtaches tache[], int i){
+
+
+    printf("modifier Un Tache  %d \n",i+1);
+    printf("|------------espace----------| \n");
+
+    printf("modifier Un titre \n");
+
+    scanf ("%s", &tache[i].titre);
+
+
+    printf("modifier Un description \n");
+    scanf ("%s", &tache[i].description);
+
+
+    printf("modifier la date de echeance \n");
+    printf("jour : \n");
+    scanf ("%d", &tache[i].dateecheance.jour);
+    printf("mois: \n");
+    scanf ("%d", &tache[i].dateecheance.mois);
+    printf("annee : \n");
+    scanf ("%d", &tache[i].dateecheance.annee);
+
+
+
+    printf("modifier une priorite : \n");
+    scanf ("%s", &tache[i].priorite);
+
+
+
 
 
 
 }
-void affichertache( Gtaches tache[], int index )
-
-{
-
-
-
-}
-
-
-
-
-
 
 main ()
 {
 
     struct Gtaches tache[max];
     int ch,nbr=0;
+     int indice;
 
 // Afficher le menu principal
 // Options :
-// 1. Ajouter une t‚che
-// 2. Afficher la liste des t‚ches
-// 3. Modifier une t‚che
-// 4. Supprimer une t‚che
-// 5. Filtrer les t‚ches par prioritÈ
-while(1){
-    printf(" --------- Afficher Le Menu Principale : --------\n");
+// 1. Ajouter une t√¢che
+// 2. Afficher la liste des t√¢ches
+// 3. Modifier une t√¢che
+// 4. Supprimer une t√¢che
+// 5. Filtrer les t√¢ches par priorit√©
+    while(1)
+    {
+        printf(" --------- Afficher Le Menu Principale : --------\n");
 
-    printf("         1-Ajouter Un Tache : \n");
-    printf("         2-Afficher la liste des taches : \n");
-    printf("         3-Modifier une tache : \n");
-    printf("         4-Supprimer une tache : \n");
-    printf("         5-Filtrer les taches par priorite: \n");
+        printf("         1-Ajouter Un Tache : \n");
+        printf("         2-Afficher la liste des taches : \n");
+        printf("         3-Modifier une tache : \n");
+        printf("         4-Supprimer une tache : \n");
+        printf("         5-Filtrer les taches par priorite: \n");
         printf("------------Donnez votre choix :------------ \n");
         scanf("%d",&ch);
 
 
-        switch(ch){
+        switch(ch)
+        {
         case 1 :
-             ajoutetache(tache,nbr);
-             nbr++;
-             break;
+            ajoutetache(tache,nbr);
+            nbr++;
+            break;
         case 2 :
-             for (int i=0 ; i<nbr ; i++)
+            //afficher les tache
+            for (int i=0 ; i<nbr ; i++)
 
- {
-    printf("La  Tache : %d \n",i+1);
+            {
+                printf("La  Tache : %d \n",i+1);
 
-    printf ("Un titre : %s \n", &tache[i].titre);
-
-
-    scanf ("Un description : %s \n", &tache[i].description);
+                printf ("Un titre : %s \n", tache[i].titre);
 
 
-    printf("la date de echeance \n");
-
-    printf ("jour : %d  \n", &tache[i].dateecheance.jour);
-    printf ("mois: %d \n", &tache[i].dateecheance.mois);
-    printf ("annee : %d \n", &tache[i].dateecheance.annee);
+                scanf ("Un description : %s \n", tache[i].description);
 
 
+                printf("la date de echeance \n");
 
-    printf ("une priorite : %s \n", &tache[i].priorite);
+                printf ("jour : %d  \n", tache[i].dateecheance.jour);
+                printf ("mois: %d \n", tache[i].dateecheance.mois);
+                printf ("annee : %d \n", tache[i].dateecheance.annee);
 
- }
-
-             break;
+                printf ("une priorite : %s \n", tache[i].priorite);
+            }
+            break;
         case 3 :
-             break;
+
+            printf("Donner moi un indice \n");
+            scanf("%d", &indice);
+            modifietache ( tache , indice);
+
+            printf( "----------Bien Modifie-----------\n");
+
+            break;
         default :
             break;
-
-
-
-
         }
-}
-
-
-
-
-
-
-
-
-
+    }
     return 0;
 }
